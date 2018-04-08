159b4d04b2af5e915f3c8cb7af74c2e9b4c13c09
Yeudiel Benjamín Vázquez Hernández <yeudiel@MacBook-Pro-de-Yeudiel.local>
2018-04-07 23:35:30 -0500

    Update Display team as a grid

--- js/participants.js
+++ js/participants.js
@@ -2,24 +2,85 @@ $(document).ready(function() {
 
     $.when(
         $.getJSON("./json/participant.json", function(d){
-            var container = $("#card-container");
-            $.each(d.participants, function(key, val){
-                var name = val.name ? val.name : " ";
-                var title = val.title ? val.title : " ";
-                var image = val.image ? val.image : "default.png";
-                container.append(   '<div class="container">' +
-                                    '<div style="position:absolute; margin: 1em; z-index: 1;">' +
-                                    '<img src="img/participants/' + image +'" class="participant-image" /> </div>' + 
-                                    '<div class="participant-card">' +
-                                        '<div class="col-sm-4"></div>' +
-                                        '<div class="col-sm-8 ">' +
-                                            '<h2 class="visible-whitespace participant-title">' + title +'</h2>' +
-                                            '<p class="visible-whitespace participant-name">'+ name +'</p>' +
-                                        '</div>'+
-                                    '</div></div><br /><br /><br />');
+            //var container = $("#card-container");
+            //let n = d.participants.length; // Length of participants
+            //let last = n % 4; // remaining cols
+            //let col = $("<div class='col-sm-3'></div>");
             
-                
+            /*$.each(d.participants, function(index, val){
+                let name = val.name || " ";
+                let title = val.title || " ";
+                let image = val.image || "default.png";
+
+
+                if (index >= n-last){
+                    let size = 12 / last;
+                    col = $("<div class='col-sm-"+ size +"'></div>")
+                } 
+
+                let card =  '<div class="card-container">' +
+                                '<div class="image">' + 
+                                    '<img src="./img/participants/"' + image + '" alt="' + name + '">' + 
+                                '</div>' +
+                                '<hr/>';
+
+                $(card).appendTo(col);
+
+                if (index % 4 === 0){
+                    let row = $("<div class='row'></div>");
+                    let cards = $(col).appendTo(row);
+                    $(cards).appendTo(container);
+                    col = $("<div class='col-sm-4'></div>");
+                }
+            });*/
+
+            let container = $("#card-container");
+            let n = d.participants.length; // Length of participants
+            let last = n % 4; // remaining cols
+            let row = $("<div></div>", {
+                "class": "row"
             });
+
+            console.log("Participants: " + n);
+
+            $.each(d.participants, function(index, participant){
+                let name = participant.name || " ";
+                let title = participant.title || " ";
+                let image = participant.image || "default.png";
+                let colWidth = (index >= n-last)?  12/last : 3;
+
+                if (index % 4 === 0 && index !== 0){
+                    $(row).appendTo(container);
+                    row = $("<div></div>", {
+                        "class": "row"
+                    });
+
+                    let col = $("<div class='col-sm-6 col-md-" + colWidth +"'>" +
+                        "<div class='card-container'>"
+                        + "<div class='image'><img src='./img/participants/" + image + "' alt='" + name + "'>"+
+                        "<hr/>" +
+                        "<div class='description'>" +
+                            "<p class='title'>" + title + "</p>" +
+                            "<p>" + name + "</p>" +
+                        "</div>" + 
+                    "</div></div></div>");
+                    $(col).appendTo(row);
+                    
+                } else {
+                    let col = $("<div class='col-sm-6 col-md-" + colWidth +"'>" +
+                        "<div class='card-container'>"
+                        + "<div class='image'>" + 
+                        "<img src='./img/participants/" + image + "' alt='" + name + "'>" +
+                        "<hr/>" +
+                        "<div class='description'>" +
+                            "<p class='title'>" + title + "</p>" +
+                            "<p>" + name + "</p>" +
+                        "</div>" + 
+                    "</div></div></div>");
+                    $(col).appendTo(row);
+                } 
+            });
+            $(row).appendTo(container);
             
         })
     )
