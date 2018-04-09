41d88de022301f1a5485354ce3ac929fb60a460c
Yeudiel Benjamín Vázquez Hernández <yeudiel@MacBook-Pro-de-Yeudiel.local>
2018-04-09 00:26:00 -0500

    Style canvas oculto en movil

--- js/participants.js
+++ js/participants.js
@@ -2,37 +2,6 @@ $(document).ready(function() {
 
     $.when(
         $.getJSON("./json/participant.json", function(d){
-            //var container = $("#card-container");
-            //let n = d.participants.length; // Length of participants
-            //let last = n % 4; // remaining cols
-            //let col = $("<div class='col-sm-3'></div>");
-            
-            /*$.each(d.participants, function(index, val){
-                let name = val.name || " ";
-                let title = val.title || " ";
-                let image = val.image || "default.png";
-
-
-                if (index >= n-last){
-                    let size = 12 / last;
-                    col = $("<div class='col-sm-"+ size +"'></div>")
-                } 
-
-                let card =  '<div class="card-container">' +
-                                '<div class="image">' + 
-                                    '<img src="./img/participants/"' + image + '" alt="' + name + '">' + 
-                                '</div>' +
-                                '<hr/>';
-
-                $(card).appendTo(col);
-
-                if (index % 4 === 0){
-                    let row = $("<div class='row'></div>");
-                    let cards = $(col).appendTo(row);
-                    $(cards).appendTo(container);
-                    col = $("<div class='col-sm-4'></div>");
-                }
-            });*/
 
             let container = $("#card-container");
             let n = d.participants.length; // Length of participants
@@ -61,7 +30,7 @@ $(document).ready(function() {
                         "<hr/>" +
                         "<div class='description'>" +
                             "<p class='title'>" + title + "</p>" +
-                            "<p>" + name + "</p>" +
+                            "<p class='participant-name'>" + name + "</p>" +
                         "</div>" + 
                     "</div></div></div>");
                     $(col).appendTo(row);
@@ -74,7 +43,7 @@ $(document).ready(function() {
                         "<hr/>" +
                         "<div class='description'>" +
                             "<p class='title'>" + title + "</p>" +
-                            "<p>" + name + "</p>" +
+                            "<p class='participant-name'>" + name + "</p>" +
                         "</div>" + 
                     "</div></div></div>");
                     $(col).appendTo(row);
